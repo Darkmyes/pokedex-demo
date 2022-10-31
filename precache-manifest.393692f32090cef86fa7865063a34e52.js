0c082b679a69cb2ee552998c861b03957604ff8c
Darkmyes <darkmyes@gmail.com>
2022-10-30 19:17:05 -0500

    first build

---
+++ precache-manifest.393692f32090cef86fa7865063a34e52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83f14e6d6d27346ba331",
+    "url": "/static/css/main.b17569ce.chunk.css"
+  },
+  {
+    "revision": "83f14e6d6d27346ba331",
+    "url": "/static/js/main.83f14e6d.chunk.js"
+  },
+  {
+    "revision": "ca2c38b71e6bc4f4d430",
+    "url": "/static/js/1.ca2c38b7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4195830a75d68d23b1d1742e49a0e21a",
+    "url": "/index.html"
+  }
+];
